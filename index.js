e82626e20476187a1901dcedc3d1728186ce31c6
BoxeR <boxer@triple-corp.com>
2019-09-02 11:42:26 +0900

    :rotating_light: prettier 적용

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 module.exports = {
   extends: [
-    "./recommends/eslint",
-    "./recommends/prettier",
-    "./recommends/react",
-    "./recommends/standard",
-    "./recommends/react",
-    "./rules/style"
+    './recommends/eslint',
+    './recommends/prettier',
+    './recommends/react',
+    './recommends/standard',
+    './recommends/react',
+    './rules/style',
   ].map(require.resolve),
   env: {
     node: true,
-    browser: true
+    browser: true,
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   settings: {
     react: {
-      createClass: "createReactClass", // Regex for Component Factory to use,
+      createClass: 'createReactClass', // Regex for Component Factory to use,
       // default to "createReactClass"
-      pragma: "React", // Pragma to use, default to "React"
-      version: "detect" // React version. "detect" automatically picks the version you have installed.
+      pragma: 'React', // Pragma to use, default to "React"
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
       // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
       // default to latest and warns if missing
       // It will default to "detect" in the future
-    }
+    },
   },
-  rules: {}
-};
+  rules: {},
+}
--- rules/typescript.js
+++ rules/typescript.js
@@ -1,12 +1,18 @@
 module.exports = {
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "no-var": "off",
-    "@typescript-eslint/no-inferrable-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }],
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-use-before-define": ["error", { functions: false, classes: true }]
-  }
-};
+    'no-var': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      { accessibility: 'no-public' },
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true },
+    ],
+  },
+}
