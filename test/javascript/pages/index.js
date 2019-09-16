ab59bd07eab05e6acc28f26aa429791c3f6b033a
BoxeR <boxer@triple-corp.com>
2019-09-16 17:12:59 +0900

    [#24] :fire: js 와 ts test 케이스 분리

---
+++ test/javascript/pages/index.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react'
+import { Container } from 'semantic-ui-react'
+
+export default class IndexPage extends PureComponent {
+  render() {
+    return <Container>Hello Triple!</Container>
+  }
+}
