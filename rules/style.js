9ff43c9c3077fcb2c3aa0e29849d507f2db85931
BoxeR <boxer@triple-corp.com>
2019-09-27 10:26:22 +0900

    :fire: comma-dangle 과 space-before-function-paren off (prettier handles)

--- rules/style.js
+++ rules/style.js
@@ -1,22 +1,3 @@
 module.exports = {
-  rules: {
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'never',
-        exports: 'never',
-        functions: 'always-multiline',
-      },
-    ],
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'never',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
-  },
+  rules: {},
 }
