666baa389bb089978d88e94c6d9512f3c2d56439
BoxeR <boxer@triple-corp.com>
2019-11-12 10:47:26 +0900

    :package: package-lock.json 업데이트

---
+++ test/javascript/prettier.js
@@ -0,0 +1,9 @@
+;(() => {
+  ;(() => {
+    const APP_URL_SCHEME = 'sss'
+    const reserveIds = ['a']
+    ;(() => {
+      window.location = `${APP_URL_SCHEME}:///billing?reserveId=${reserveIds[0]}`
+    })()
+  })()
+})()
