9afc9c6143a46178518c754dc65b2b5ea2071e10
woody <woody@triple-corp.com>
2020-01-29 13:12:06 +0900

    Feature: stylelint 세팅
    styled-components 관련 패키지도 함께 설치합니다.

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  processors: [
+    'stylelint-processor-styled-components',
+  ],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-styled-components',
+  ]
+}
