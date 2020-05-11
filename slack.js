659578fe501d1a2a10d037a0874762111c9cf494
BoxeR <boxer@triple-corp.com>
2020-05-11 21:08:56 +0900

    :wrench: fallback text 추가

--- slack.js
+++ slack.js
@@ -9,21 +9,23 @@ const {
   SLACK_TOPIC,
   GITHUB_REF,
   GITHUB_EVENT_NAME,
+  GITHUB_REPOSITORY,
   GITHUB_ACTIONS_URL,
-  TRIGGER_USER,
-  TRIGGER_USER_AVATAR,
+  SLACK_AUTHOR_NAME,
+  SLACK_AUTHOR_ICON,
 } = process.env
 
 const template = {
   "channel": SLACK_CHANNEL,
   "icon_emoji": SLACK_ICON_EMOJI,
   "username": SLACK_USERNAME,
-  "text": `*${SLACK_TITLE}* - <${GITHUB_ACTIONS_URL}|[detail]>`,
+  "text": `*${SLACK_TITLE}* - <${GITHUB_ACTIONS_URL}|[${GITHUB_REPOSITORY}]>`,
   "attachments":[{
+    "fallback": "GitHub Action workflow done.",
     "color": SLACK_COLOR,
-    "author_name": TRIGGER_USER,
-    "author_icon": TRIGGER_USER_AVATAR,
-    "author_link": `https://github.com/${TRIGGER_USER}`,
+    "author_name": SLACK_AUTHOR_NAME,
+    "author_icon": SLACK_AUTHOR_ICON,
+    "author_link": `https://github.com/${SLACK_AUTHOR_NAME}`,
     "fields":[{
       "title": "Topic",
       "value": SLACK_TOPIC,
