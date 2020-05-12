0de133ccd23985e3eb20a20673ead0f7eaa6f970
BoxeR <boxer@triple-corp.com>
2020-05-12 10:35:39 +0900

    :recycle: node.js 스크립트에서 직접 curl 을 실행합니다.

--- .github/workflows/notifier.js
+++ .github/workflows/notifier.js
@@ -1,3 +1,5 @@
+const exec = require('child_process').exec;
+
 const {
   SLACK_CHANNEL,
   SLACK_WEBHOOK,
@@ -15,6 +17,22 @@ const {
   SLACK_AUTHOR_ICON,
 } = process.env
 
+const COLOR_PRESET = {
+  "success": "#36a64f",
+  "fail": "#fd2e69",
+  "good": "#36a64f",
+  "bad": "#fd2e69",
+  "green": "#36a64f",
+  "blue": "#368fff",
+  "mint": "#26cec2",
+  "red": "#fd2e69",
+  "purple": "#975ffe",
+  "emerald": "#0dd0af",
+  "gray": "#3a3a3a",
+  "brightgray": "#efefef",
+  "orange": "#ff9623",
+}
+
 const template = {
   "channel": SLACK_CHANNEL,
   "icon_emoji": SLACK_ICON_EMOJI,
@@ -22,7 +40,7 @@ const template = {
   "text": `*${SLACK_TITLE}* - <${GITHUB_ACTIONS_URL}|[${GITHUB_REPOSITORY}]>`,
   "attachments":[{
     "fallback": "GitHub Action workflow done.",
-    "color": SLACK_COLOR,
+    "color": COLOR_PRESET[SLACK_COLOR] || SLACK_COLOR,
     "author_name": SLACK_AUTHOR_NAME,
     "author_icon": SLACK_AUTHOR_ICON,
     "author_link": `https://github.com/${SLACK_AUTHOR_NAME}`,
@@ -44,4 +62,14 @@ const template = {
   }]
 }
 
-console.log(JSON.stringify(template))
+const payload = JSON.stringify(template)
+const cmd = `curl -X POST --data-urlencode 'payload=${payload}' ${SLACK_WEBHOOK}`
+
+exec(cmd, function (error, stdout, stderr) {
+  console.log(stdout);
+  console.log(stderr);
+
+  if (error !== null) {
+    console.error(error);
+  }
+})
