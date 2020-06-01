b35458a84a86ada3948662fb89a5f2b2d592cda0
woody <woody@triple-corp.com>
2020-06-01 17:04:26 +0900

    Feature: add optional chaining and nullish coersion syntax

--- test/typescript/src/module-1/index.ts
+++ test/typescript/src/module-1/index.ts
@@ -5,3 +5,10 @@ export type { Foo } from './types'
 export interface Wrapper {
   internal: InternalFoo
 }
+
+export function test(param: string | undefined | null): string {
+  const handler = (param?: { isValid: boolean }) =>
+    param?.isValid ? 'test' : 'fail'
+
+  return param ?? handler()
+}
