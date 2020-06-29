c11167634eb2bfa008d98c6f83fda8217af3889e
BoxeR <boxer@triple-corp.com>
2020-06-29 11:40:14 +0900

    :package: eslint-config-standard 설치

--- recommends/base.js
+++ recommends/base.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: [
-    './eslint',
-    './standard',
-    './prettier',
-    './react',
-    './react-hooks',
-  ].map(require.resolve),
+    'standard',
+    ...([
+      './eslint',
+      // './standard',
+      './prettier',
+      './react',
+      './react-hooks',
+    ].map(require.resolve)),
+  ]
 }
