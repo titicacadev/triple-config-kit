38dc7363e3b9e676dec2b89300367417ad8b569e
woody <woody@triple-corp.com>
2021-01-27 14:35:22 +0900

    ✨ react/self-closing-comp 규칙 추가

--- rules/react.js
+++ rules/react.js
@@ -1,5 +1,12 @@
 module.exports = {
   rules: {
     'react/prop-types': 0, // See issue #28
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
   },
 }
