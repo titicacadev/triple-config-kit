5eed5ff290d852a1b40a516283948df7355a896f
woody <woody@triple-corp.com>
2021-04-27 14:04:12 +0900

    🔥 중복된 규칙 설정 제거

--- rules/style.js
+++ rules/style.js
@@ -2,13 +2,6 @@ module.exports = {
   rules: {
     camelcase: ['error', { properties: 'always' }],
     curly: ['error', 'all'],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', ['parent', 'sibling', 'index']],
-        'newlines-between': 'always',
-      },
-    ],
     'no-unused-vars': [
       'error',
       { ignoreRestSiblings: true, argsIgnorePattern: '^_+$' },
