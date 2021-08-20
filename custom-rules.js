a42b0be28ad06a38ae3e703600016e1f3f16ca15
woody <woody@triple-corp.com>
2021-08-20 20:57:38 +0900

    ✨ eslint:recommended 확장 사용
    새로 추가된 규칙이 있어 스냅샷을 업데이트합니다. 기존 규칙에 영향이
    없도록 off로 추가합니다.

--- custom-rules.js
+++ custom-rules.js
@@ -1,3 +1,8 @@
 module.exports = {
-  rules: {},
+  rules: {
+    // eslint:recommended에서 새로 추가된 규칙
+    'no-dupe-else-if': ['off'],
+    'no-import-assign': ['off'],
+    'no-setter-return': ['off'],
+  },
 }
