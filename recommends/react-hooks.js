5a2c31db32a69dfd76b7d8a6b38707aeb5ac3b9c
woody <woody@triple-corp.com>
2021-08-26 18:25:36 +0900

    🔥 recommends/react-hooks에서 스냅샷에 영향 주지 않는 규칙 제거

--- recommends/react-hooks.js
+++ recommends/react-hooks.js
@@ -1,8 +1,3 @@
 module.exports = {
-  plugins: ['react-hooks'],
   extends: ['plugin:react-hooks/recommended'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
 }
