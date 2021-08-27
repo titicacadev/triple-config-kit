b822bbb87c580b4a7f7c8f42c3bf107895476a8a
woody <woody@triple-corp.com>
2021-08-27 15:30:15 +0900

    💥 jsx-a11y eslint 플러그인 추천 설정 사용
    이전엔 jsx-a11y 플러그인 설정이 안 되어있었기 때문에 모든 규칙을 검사하지 않았습니다.
    이번 커밋으로 새로운 규칙이 추가되기 때문에 Breaking Change입니다.
    
    Resolves #104

--- typescript.js
+++ typescript.js
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'standard',
     ...['./rules/base'].map(require.resolve),
     'plugin:@typescript-eslint/recommended',
