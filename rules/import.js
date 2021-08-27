e5cfd6049730e0d29914a0a965c9874d8050e858
woody <woody@triple-corp.com>
2021-08-27 19:13:21 +0900

    💥 import 플러그인의 추천 규칙 활성화

--- rules/import.js
+++ rules/import.js
@@ -16,13 +16,6 @@ module.exports = {
     ],
     'import/newline-after-import': 'error',
 
-    // 추천 규칙에서 추가된 규칙 비활성화
-    'import/default': 'off',
-    'import/named': 'off',
-    'import/namespace': 'off',
-    'import/no-named-as-default': 'off',
-    'import/no-named-as-default-member': 'off',
-    'import/no-unresolved': 'off',
     // 추천 규칙에서 warning이지만 기존에 error였던 규칙 보정
     'import/no-duplicates': 'error',
   },
