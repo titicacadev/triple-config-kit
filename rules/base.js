cbed0c158325be01e17a8e9b331a49cb55728613
woody <woody@triple-corp.com>
2021-08-27 19:11:39 +0900

    💥 eslint:recommended에서 추가한 규칙 활성화

--- rules/base.js
+++ rules/base.js
@@ -21,11 +21,6 @@ module.exports = {
     'object-shorthand': ['error', 'always'],
     'require-atomic-updates': 'error',
 
-    // eslint:recommended에서 새로 추가된 규칙은 비활성화
-    'no-dupe-else-if': ['off'],
-    'no-import-assign': ['off'],
-    'no-setter-return': ['off'],
-
     // standard 추천 규칙 모음을 사용하면서 새로 추가된 규칙 비활성화
     'array-callback-return': 'off',
     'default-case-last': 'off',
