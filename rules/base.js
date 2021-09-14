197726f8b56c230a1b880a1ea0d20c6d7835ee33
woody <woody@triple-corp.com>
2021-09-14 15:18:51 +0900

    💥 standard 플러그인의 추천 규칙에서 추가한 규칙 활성화

--- rules/base.js
+++ rules/base.js
@@ -23,21 +23,6 @@ module.exports = {
     'object-shorthand': ['error', 'always'],
     'require-atomic-updates': 'error',
 
-    // standard 추천 규칙 모음을 사용하면서 새로 추가된 규칙 비활성화
-    'array-callback-return': 'off',
-    'default-case-last': 'off',
-    'dot-notation': 'off',
-    'import/no-absolute-path': 'off',
-    'no-loss-of-precision': 'off',
-    'no-unreachable-loop': 'off',
-    'no-useless-backreference': 'off',
-    'no-var': 'off',
-    'node/handle-callback-err': 'off',
-    'node/no-exports-assign': 'off',
-    'node/no-new-require': 'off',
-    'node/no-path-concat': 'off',
-    'prefer-regex-literals': 'off',
-
     // standard 추천 규칙 모음을 사용하면서 변경된 규칙설정 보정
     'no-empty': ['error', { allowEmptyCatch: false }],
     'use-isnan': [
