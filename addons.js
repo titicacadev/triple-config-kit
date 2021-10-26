002f467a4d7dae9d9bb4f30fd33d5cbc43b4844e
woody <woody@triple-corp.com>
2021-10-26 11:01:59 +0900

    ✨ project 파라미터의 존재 유무에 따라 타입 체크 규칙 추가 여부를 조절
    별도의 함수를 두지 않고 createConfig 함수에서 project 파라미터의 존재 여부에 따라 규칙을 추가합니다.

--- addons.js
+++ addons.js
@@ -3,7 +3,7 @@ const {
   commonExcludes,
 } = require('./rules/typescript/naming-convention')
 
-function createConfig({ project, allowedNames = [] } = {}) {
+function createConfig({ allowedNames = [], project } = {}) {
   const namingConventionIgnoreRegEx = `^(${[
     ...commonExcludes,
     ...allowedNames,
@@ -14,26 +14,26 @@ function createConfig({ project, allowedNames = [] } = {}) {
     overrides: [
       {
         files: ['*.ts', '*.tsx'],
-        settings: { 'import/resolver': { typescript: { project } } },
         rules: {
           '@typescript-eslint/naming-convention': getRules({
             regex: namingConventionIgnoreRegEx,
           }),
         },
       },
+      ...(project
+        ? [
+            {
+              files: ['*.ts', '*.tsx'],
+              extends: [
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+              ],
+              parserOptions: { project },
+              settings: { 'import/resolver': { typescript: { project } } },
+            },
+          ]
+        : []),
     ],
   }
 }
 
-function withTypeCheckingRules(config, { project }) {
-  return {
-    ...config,
-    extends: [
-      ...config.extends,
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
-    parserOptions: { ...config.parserOptions, project },
-  }
-}
-
-module.exports = { createConfig, withTypeCheckingRules }
+module.exports = { createConfig }
