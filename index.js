6d3cd229ffe9ca97e1e09d9b3e4e31ead2a11a04
woody <woody@triple-corp.com>
2021-10-28 13:26:43 +0900

    🔥 타입 체크는 규칙 비활성화
    타입을 체크하려면 tsconfig.json 정보를 알아야합니다.
    그런데 모노레포 환경에서 각 패키지의 tsconfig.json을 제공하면 메모리 부족 문제가 발생합니다.
    반대로 root의 tsconfig.json을 제공하면 이웃 패키지의 타입 참조를 알지 못하는 문제가 있습니다.
    이 문제를 해결할 수 있을 때까지 타입을 확인하는 규칙을 공통화하지 않습니다.

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:import/typescript',
         ...['./rules/typescript', './rules/prettier'].map(require.resolve),
       ],
