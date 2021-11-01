b01ddea440083fdbb284ba7e29f2b48c4d31896d
woody <woody@triple-corp.com>
2021-11-01 14:55:48 +0900

    ✅ create-config 테스트 코드 추가

---
+++ test/create-config.test.js
@@ -0,0 +1,40 @@
+const { ESLint } = require('eslint')
+
+const createConfig = require('../create-config')
+
+test('eslint createConfig javascript', async () => {
+  const overrideConfig = createConfig({
+    allowedNames: ['_triple_mock_value'],
+  })
+  const eslint = new ESLint({
+    extensions: ['js', 'jsx'],
+    overrideConfig,
+    useEslintrc: false,
+  })
+
+  const config = await eslint.calculateConfigForFile('./mock.js')
+
+  expect(config).toMatchSnapshot({
+    parser: expect.stringMatching(/@babel\/eslint-parser/),
+  })
+})
+
+test('eslint createConfig typescript', async () => {
+  const overrideConfig = createConfig({
+    allowedNames: ['_triple_mock_value'],
+    project: './tsconfig.json',
+    tsconfigRootDir: '~/mock-dir-name',
+    enableTypeCheck: true,
+  })
+  const eslint = new ESLint({
+    extensions: ['ts', 'tsx'],
+    overrideConfig,
+    useEslintrc: false,
+  })
+
+  const config = await eslint.calculateConfigForFile('./mock.ts')
+
+  expect(config).toMatchSnapshot({
+    parser: expect.stringMatching(/@typescript-eslint/),
+  })
+})
