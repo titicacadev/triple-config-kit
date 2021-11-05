e3be8c928b6a67a554765bcea3ca827833533cef
Heeryong Kang <drakang4@gmail.com>
2021-11-05 20:18:42 +0900

    feat: migrate stylelint to 14

--- stylelint.js
+++ stylelint.js
@@ -1,7 +1,14 @@
 module.exports = {
-  processors: ['stylelint-processor-styled-components'],
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-styled-components',
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  overrides: [
+    {
+      files: ['**/*.{js,ts,tsx}'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+      extends: [
+        'stylelint-config-standard',
+        'stylelint-config-styled-components',
+        'stylelint-config-prettier',
+      ],
+    },
   ],
 }
