81e9794d23b5e02ab6eaa02f5e020d85a7d10e78
Heeryong Kang <drakang4@gmail.com>
2021-11-08 15:21:35 +0900

    feat: stylelint rule 추가

--- stylelint.js
+++ stylelint.js
@@ -1,14 +1,18 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'alpha-value-notation': 'number',
+    'color-function-notation': 'legacy',
+  },
   overrides: [
     {
       files: ['**/*.{js,ts,tsx}'],
       customSyntax: '@stylelint/postcss-css-in-js',
-      extends: [
-        'stylelint-config-standard',
-        'stylelint-config-styled-components',
-        'stylelint-config-prettier',
-      ],
+      extends: ['stylelint-config-styled-components'],
+      rules: {
+        'function-name-case': null,
+        'value-keyword-case': null,
+      },
     },
   ],
 }
