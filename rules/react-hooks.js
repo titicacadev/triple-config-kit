2a0fbfe92e37bd1889d47273a883e5a0bdd6236e
woody <woody@triple-corp.com>
2021-11-09 11:18:33 +0900

    ✨ react-hooks/exhaustive-deps 워닝으로 변경
    Resolves #121
    
    https://github.com/titicacadev/eslint-config-triple/issues/121

--- rules/react-hooks.js
+++ rules/react-hooks.js
@@ -1,8 +1,5 @@
 module.exports = {
   rules: {
-    /**
-     * https://github.com/titicacadev/eslint-config-triple/issues/32
-     */
-    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
