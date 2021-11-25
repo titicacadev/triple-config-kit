23c3e29c8e45ebc26ffdffe3cf622ec104bf0261
woody <woody@triple-corp.com>
2021-11-26 07:53:15 +0900

    ✨ async/await 선호하는 규칙 활성화

--- rules/promise.js
+++ rules/promise.js
@@ -1,5 +1,7 @@
 module.exports = {
   rules: {
     'promise/catch-or-return': ['error', { allowFinally: true }],
+    'promise/prefer-await-to-callbacks': 'error',
+    'promise/prefer-await-to-then': 'error',
   },
 }
