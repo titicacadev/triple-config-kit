913c211ba39b34b5a4a9b6371f573b8249ecb78d
Heeryong Kang <drakang4@gmail.com>
2023-02-14 15:31:53 +0900

    chore(eslint-config-triple): 불필요한 파일 제거

--- packages/eslint-config-triple/rules/prettier.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['prettier'],
-  rules: {
-    /**
-     * prettier에서 비활성화하지만 필요에 의해 다시 활성화하는 규칙
-     */
-    curly: ['error', 'all'],
-  },
-}
--- packages/eslint-config-triple/rules/promise.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rules: {
-    'promise/catch-or-return': ['error', { allowFinally: true }],
-    'promise/prefer-await-to-callbacks': 'error',
-    'promise/prefer-await-to-then': 'error',
-  },
-}
