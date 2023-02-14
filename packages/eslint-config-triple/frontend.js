5538ef936da116c834e09fcfb34705a243ad2a3e
Heeryong Kang <drakang4@gmail.com>
2023-02-14 15:18:34 +0900

    chore(eslint-config-triple): 컨픽 설정을 단순하게 변경

--- packages/eslint-config-triple/frontend.js
+++ packages/eslint-config-triple/frontend.js
@@ -1,39 +1,57 @@
-const {
-  createNamingConventionOptions,
-  createNamingConventionConfig,
-  addReactComponentNamingConvention,
-} = require('./rules/typescript/naming-convention')
-
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
+  env: {
+    browser: true,
+  },
   extends: [
-    ...[
-      './plugin-configs/react',
-      './plugin-configs/react-hooks',
-      './plugin-configs/jsx-a11y',
-    ].map(require.resolve),
+    'plugin:react/recommended',
+    'plugin:react-jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11yrecommended',
     'standard-jsx',
-    ...[
-      './plugin-configs/react-jsx-runtime',
-      './rules/react',
-      './rules/react-hooks',
-      './rules/prettier',
-    ].map(require.resolve),
   ],
   settings: {
     react: {
       version: 'detect',
     },
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/naming-convention': createNamingConventionConfig({
-          options: addReactComponentNamingConvention(
-            createNamingConventionOptions(),
-          ),
-        }),
+  rules: {
+    'react/display-name': 'error',
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'react/prop-types': 0,
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
       },
-    },
-  ],
+    ],
+  },
+
+  // extends: [
+  //   ...[
+  //     './plugin-configs/react',
+  //     './plugin-configs/react-hooks',
+  //     './plugin-configs/jsx-a11y',
+  //   ].map(require.resolve),
+  //   'standard-jsx',
+  //   ...[
+  //     './plugin-configs/react-jsx-runtime',
+  //     './rules/react',
+  //     './rules/react-hooks',
+  //     './rules/prettier',
+  //   ].map(require.resolve),
+  // ],
+  // overrides: [
+  //   {
+  //     files: ['*.ts', '*.tsx'],
+  //     rules: {
+  //       '@typescript-eslint/naming-convention': createNamingConventionConfig({
+  //         options: addReactComponentNamingConvention(
+  //           createNamingConventionOptions(),
+  //         ),
+  //       }),
+  //     },
+  //   },
+  // ],
 }
