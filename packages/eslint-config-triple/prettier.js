ea30663264bccb54a1b24a8469a4921ef92fe8ac
choisohyun <sohyun4283@naver.com>
2023-12-12 13:15:49 +0900

    fix: prettier rules에도 curly 추가

--- packages/eslint-config-triple/prettier.js
+++ packages/eslint-config-triple/prettier.js
@@ -1,4 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['prettier'],
+  rules: {
+    /**
+     * prettier에서 비활성화하지만 필요에 의해 다시 활성화하는 규칙
+     */
+    curly: ['error', 'all'],
+  },
 }
